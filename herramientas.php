479cff7b01b671b76e5a6cfab0a938885ba1a9dd
David Dzul <davidf.dzul@gmail.com>
2020-05-15 20:18:30 -0500

    Nueva página

---
+++ herramientas.php
@@ -0,0 +1,17 @@
+<?php
+	require_once 'class/Template.php';
+	$Template = new Template('');
+	$Template->bodyTop();	
+?>
+<div class="content">
+  <div class="row">
+    <div class="col-md-12">
+      <h1>Página principal</h1>
+    </div>
+  </div>  
+</div>
+
+<?php
+	$Template->bodyButton();
+?>
+<script src=""></script>
