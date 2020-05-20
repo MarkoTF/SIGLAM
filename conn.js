325e525234f6f645e73cb5f7b8017c5234501da4
WilliamEXP <65380122+WilliamEXP@users.noreply.github.com>
2020-05-20 18:13:11 -0500

    ultima version

--- conn.js
+++ conn.js
@@ -11,6 +11,55 @@ firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error
 }).then(function(resolve){
   if (resolve != null){
     window.location = "inicio.php"
+    alertify.success("Error en sus credenciales");
+  }
+  else
+    {alertify.error("Correo o contraseña incorrectos");}
+});
+}
+
+function iniciarroot(){  
+  var errorCode=""
+  var errorMessage = ""
+var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // ...
+}).then(function(resolve){
+  if (resolve != null){
+    window.location = "../inicio.php"
   }
 });
+}
+
+function cerrar(){  
+firebase.auth().signOut().then(function() {
+  window.location = "index.html"
+}).catch(function(error) {
+  // An error happened.
+});
+}
+
+function reset(){
+
+var emailAddress = document.getElementById("emailadress").value;
+
+firebase.auth().sendPasswordResetEmail(emailAddress).then(function() {
+  
+}).catch(function(error) {
+  // An error happened.
+});
+
+}
+function confirmar(){
+
+  alertify.confirm('Salir', '¿Seguro que deseas salir?', function(){ 
+    cerrar();
+    alertify.success('Ok') 
+  }
+                , function(){});
+
 }
